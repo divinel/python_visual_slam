b1dbf6e9d16d92aae90291d62710f522f4d3d998
BrianLi <bailin.github@gmail.com>
2020-04-06 12:38:31 +0800

    [Feature] Add orb extractor and corner extractor

--- slam.py
+++ slam.py
@@ -1,5 +1,6 @@
-import data_loader, displayer
-import sys
+import data_loader, displayer, image_processing
+import sys, copy
+import cv2 as cv
 
 def main():
     data_folder = sys.argv[1]
@@ -8,10 +9,19 @@ def main():
     print("{image_size} images are loaded".format(image_size = len(img_files)))
     image_loader = data_loader.ImageLoader(img_files)
     image_displayer = displayer.Displayer("data")
+    feature_extractor = image_processing.FeatureExtractor(quality_level = 0.01, min_dist = 5)
+
     while not image_loader.empty():
         img = image_loader.get_next_image()
-        if img is not None:
-            image_displayer.display(img, 100)
+        disp_img = img.copy()
+        gray_img = image_processing.color2gray(img)
+        kps = feature_extractor.extract(gray_img, 1000)
+        for kp in kps:
+            u,v = kp
+            cv.circle(disp_img, (u,v), radius=2, color=(0, 255, 0), thickness=1)
+
+        if disp_img is not None:
+            image_displayer.display(disp_img, 10)
 
 
 if __name__ == "__main__":
